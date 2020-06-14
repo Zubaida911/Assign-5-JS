51fa29849c7b6e5a529db7abec52a8f94abea7cd
Zubaida911 <65039982+Zubaida911@users.noreply.github.com>
2020-06-14 16:46:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+                   //Assignment#1 Q#1;
+// alert("Welcome to Digimart");
+
+                   //Assignment#1 Q#2;
+//alert("Error! Please enter a valid password");
+
+                   //Assignment#1 Q#3;
+//alert("Welcome to Digimart\nHappy Digital Marketing!");
+
+                  //Assignment#1 Q#4;
+alert("Welcome to Digimart");
+alert("Happy Digital Marketing!");
